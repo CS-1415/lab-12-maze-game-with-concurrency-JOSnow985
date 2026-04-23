ea71ff4a9efca805239ae7b5f63459588723f785
JOSnow985 <jaden.olvera@students.snow.edu>
2026-04-23 17:15:46 -0600

    Intro & Story

--- Lab12.Main/Program.cs
+++ Lab12.Main/Program.cs
@@ -1,2 +1,6 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Jaden Olvera, 4/24/26, Lab 12 Maze Game with Concurrency
+Console.WriteLine("Welcome to a slightly-less-basic, super-cool maze game with concurrency, the guards will move independently of you!");
+Console.WriteLine("--- Rules Placeholder ---");
+Console.WriteLine("Insert Coin to Play...");
+
+Console.ReadKey();
