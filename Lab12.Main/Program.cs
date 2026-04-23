c1e7a8ed5862a4671106184cb0657be81d2867d5
JOSnow985 <jaden.olvera@students.snow.edu>
2026-04-23 17:48:54 -0600

    Basic controls with a do-while

--- Lab12.Main/Program.cs
+++ Lab12.Main/Program.cs
@@ -14,3 +14,35 @@ foreach(string line in mapStrings)
 {
     Console.WriteLine(line);
 }
+Console.ReadKey();
+
+// Loop until the key pressed is Escape, print proposed directions
+ConsoleKey lastKey = ConsoleKey.None;
+Console.Clear();
+do
+{
+    lastKey = Console.ReadKey(true).Key;
+    switch (lastKey)
+    {
+        case ConsoleKey.W:
+        case ConsoleKey.UpArrow:
+            Console.WriteLine("Up");
+            break;
+        case ConsoleKey.D:
+        case ConsoleKey.RightArrow:
+            Console.WriteLine("Right");
+            break;
+        case ConsoleKey.S:
+        case ConsoleKey.DownArrow:
+            Console.WriteLine("Down");
+            break;
+        case ConsoleKey.A:
+        case ConsoleKey.LeftArrow:
+            Console.WriteLine("Left");
+            break;
+        case ConsoleKey.Escape:
+            break;
+        default:
+            break;
+    }
+} while (lastKey != ConsoleKey.Escape);
