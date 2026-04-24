786eb612a372b23b24c7bb146ac5c5086cf37cee
JOSnow985 <jaden.olvera@students.snow.edu>
2026-04-24 16:24:21 -0600

    LoadEntityList() method and property for the created list

--- Lab12/Map.cs
+++ Lab12/Map.cs
@@ -4,10 +4,12 @@ public class Map
 {
     public int Height => Layout.Count;        // Map's height boundary is the count of the list of list of cells
     public int Width => Layout[0].Count;      // Map's width boundary is the count of cells in the first list of cells
-    public List<List<char>> Layout { get; private set; }
+    public List<List<char>> Layout { get; private set; } = [];
+    public List<Entity> MapEntities { get; private set; }
     public Map(string mapfile)
     {
         Layout = LoadMapFile(mapfile);
+        MapEntities = LoadEntityList();
     }
     private static List<List<char>> LoadMapFile(string filepath)
     {
@@ -26,6 +28,22 @@ public class Map
         }
 
         return map;
+    }
+    private List<Entity> LoadEntityList()
+    {
+        List<Entity> entities = [];
+        for(int y = 0; y < Layout.Count; y++)
+        {
+            for(int x = 0; x < Layout[y].Count; x++)
+            {
+                if (Layout[y][x] == '%')
+                {
+                    entities.Add(new Guard(this, x, y));
+                }
+            }
+        }
+
+        return entities;
     }
     public void ChangeCell(int col, int row, char c) => Layout[row][col] = c;
     public void DisableGateSymbols()
