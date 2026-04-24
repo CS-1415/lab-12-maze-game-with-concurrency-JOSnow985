ba0879ab6b87a5f31185e9849e847c3be58c4083
JOSnow985 <jaden.olvera@students.snow.edu>
2026-04-23 18:00:16 -0600

    TryMove method

--- Lab12/Helpers.cs
+++ Lab12/Helpers.cs
@@ -1,6 +1,14 @@
 ﻿namespace Lab12;
 
-public class Helpers
+public static class Helpers
 {
-
+    public static bool TryMove(int targetX, int targetY)
+    {
+        if (targetX > Console.BufferWidth || targetX < 0)
+            return false;
+        if (targetY > Console.BufferHeight || targetY < 0)
+            return false;
+        // If the target coordinates don't fail the two previous conditions, return true
+        return true;
+    }
 }
