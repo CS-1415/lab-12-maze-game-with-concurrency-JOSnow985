2b7be056200e18cf53eccf658ffc4db56a03a585
JOSnow985 <jaden.olvera@students.snow.edu>
2026-04-23 18:09:17 -0600

    stay on the screen

--- Lab12.Main/Program.cs
+++ Lab12.Main/Program.cs
@@ -1,6 +1,8 @@
 ﻿// Jaden Olvera, 4/24/26, Lab 12 Maze Game with Concurrency
 
 // Intro & Story
+using Lab12;
+
 Console.WriteLine("Welcome to a slightly-less-basic, super-cool maze game with concurrency, the guards will move independently of you!");
 Console.WriteLine("--- Rules Placeholder ---");
 Console.WriteLine("Insert Coin to Play...");
@@ -19,6 +21,9 @@ Console.ReadKey();
 // Loop until the key pressed is Escape, print proposed directions
 ConsoleKey lastKey = ConsoleKey.None;
 Console.Clear();
+
+// placeholder "player" coordinates
+(int X, int Y) player = (0,0);
 do
 {
     lastKey = Console.ReadKey(true).Key;
@@ -26,19 +31,45 @@ do
     {
         case ConsoleKey.W:
         case ConsoleKey.UpArrow:
-            Console.WriteLine("Up");
+            if (Helpers.TryMove(player.X, player.Y - 1))
+            {
+                player.Y -= 1;
+                Console.WriteLine($"Up - now at ({player.X},{player.Y})");
+            }
+            else
+                Console.WriteLine($"Up - still at ({player.X},{player.Y})");
             break;
         case ConsoleKey.D:
         case ConsoleKey.RightArrow:
-            Console.WriteLine("Right");
+            if (Helpers.TryMove(player.X + 1, player.Y))
+            {
+                player.X += 1;
+                Console.WriteLine($"Right - now at ({player.X},{player.Y})");
+            }
+            else
+                Console.WriteLine($"Right - still at ({player.X},{player.Y})");
             break;
         case ConsoleKey.S:
         case ConsoleKey.DownArrow:
             Console.WriteLine("Down");
+            if (Helpers.TryMove(player.X, player.Y + 1))
+            {
+                player.Y += 1;
+                Console.WriteLine($"Down - now at ({player.X},{player.Y})");
+            }
+            else
+                Console.WriteLine($"Down - still at ({player.X},{player.Y})");
             break;
         case ConsoleKey.A:
         case ConsoleKey.LeftArrow:
             Console.WriteLine("Left");
+            if (Helpers.TryMove(player.X - 1, player.Y))
+            {
+                player.X -= 1;
+                Console.WriteLine($"Left - now at ({player.X},{player.Y})");
+            }
+            else
+                Console.WriteLine($"Left - still at ({player.X},{player.Y})");
             break;
         case ConsoleKey.Escape:
             break;
